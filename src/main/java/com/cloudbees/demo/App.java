1bcd0f901e62f20e4281d64f8ebbe9fc570a6305
Udaypal Aarkoti <uday@cloudbees.com>
2014-11-03 16:14:30 -0600

    Walmart 5

--- src/main/java/com/cloudbees/demo/App.java
+++ src/main/java/com/cloudbees/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Walmart 4!" );
+        System.out.println( "Hello Walmart 5!" );
     }
 }
