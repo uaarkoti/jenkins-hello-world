3e901eae7f91fe65658003a447363840aa332b9a
Udaypal Aarkoti <uaarkoti@gmail.com>
2015-09-21 17:18:09 -0700

    Updated to test fast archiver

--- src/main/java/com/cloudbees/demo/App.java
+++ src/main/java/com/cloudbees/demo/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        App.print();
+    }
+    
+    public static void print() {
         System.out.println( "Hello ChefConf" );
     }
 }
