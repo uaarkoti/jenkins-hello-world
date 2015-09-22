71bbc9b5fa0a36c6aac8a3b49fcfa9126d8f378c
Udaypal Aarkoti <uaarkoti@gmail.com>
2015-09-21 18:11:16 -0700

    Reverted changes for fast archiver

--- src/main/java/com/cloudbees/demo/App.java
+++ src/main/java/com/cloudbees/demo/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        App.print();
-    }
-    
-    public static void print() {
         System.out.println( "Hello ChefConf" );
     }
 }
